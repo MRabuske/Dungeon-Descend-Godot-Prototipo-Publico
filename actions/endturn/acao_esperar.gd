2dce78b1586c1e2c1940e7aac56e4a3acffef4e5
MRabuske <mbfrabuske@gmail.com>
2026-08-29 14:57:52 -0300

    Delete actions directory

--- actions/endturn/acao_esperar.gd
+++
@@ -1,9 +0,0 @@
-class_name AcaoEsperar
-extends ActionData
-
-func _init() -> void:
-	label       = "Esperar"
-	action_type = Type.END_TURN
-	shape       = SHAPE_TRIANGLE
-	color_idx   = COLOR_WAIT
-	icon 		= preload("res://assets/ui/icons/acoes/acao_esperar.png")
--- actions/endturn/acao_fugir.gd
+++
@@ -1,9 +0,0 @@
-class_name AcaoFugir
-extends ActionData
-
-func _init() -> void:
-	label       = "Fugir"
-	action_type = Type.END_TURN
-	shape       = SHAPE_TRIANGLE
-	color_idx   = COLOR_FLEE
-	icon 		= preload("res://assets/ui/icons/acoes/acao_fugir.png")
